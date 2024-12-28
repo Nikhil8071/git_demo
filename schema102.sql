602f31f8c54f80b42f0da1d78bce69ae13a4646a
Nikhil Kumar <nikhil4917305@gmail.com>
2024-12-28 17:03:21 +0530

    added schema

---
+++ schema102.sql
@@ -0,0 +1,19 @@
+drop table if exists customer;
+create table customer
+(
+	id		int primary key,
+	name	varchar(30),
+	email	varchar(50)
+);
+
+
+drop table if exists bike;
+create table bike
+(
+	id					int primary key,
+	model				varchar(50),
+	category			varchar(50),
+	price_per_hour		decimal,
+	price_per_day		decimal,
+	status				varchar(20)
+);
